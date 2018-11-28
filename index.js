7d55cd541f65e1c1d3a6037525c5ef87426ef0ba
Vignesh Sankaran <developer@sankaran.io>
2018-11-28 15:23:43 +1100

    Set up first PTV API call

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 "use strict";
 
-var app = require("express");
+const express = require("express");
+const ptv = require("ptv-api");
+const app = express();
 
-app.get("/routes/:stop_id", (req, res, next) => {
-  next();
+const devId = "1000649";
+const apiKey = "47235180-b369-11e5-a65e-029db85e733b";
+const ptvClient = ptv(devId, apiKey);
+
+app.get("/routesForStop", (req, res, next) => {
+  if (!req.query.hasOwnProperty("stop_id")) {
+    res.sendStatus(404);
+    return;
+  }
+
+  // Hit PTV for departures, collect ids
+  ptvClient
+    .then(apis => {
+      return apis.Departures.Departures_GetForStop({
+        route_type: 0,
+        stop_id: req.query.stop_id
+      });
+    })
+    .then(ptvRes => {
+      res.send(ptvRes.body);
+      return;
+    })
+    .catch(_ => {
+      res.sendStatus(403);
+      return;
+    });
+
+  // Hit PTV for route names, match against route ids
+  // Create JSON structure
+  // Return JSON
 });
 
-app.listen(process.ENV.port || 3000);
+app.listen(3000, () => console.log("Listening on port 3000!"));
