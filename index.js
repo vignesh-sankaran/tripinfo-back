dcd3ef8e5e1f7c6b5ee91461a850970b26b06d51
Vignesh Sankaran <developer@sankaran.io>
2018-11-29 23:25:18 +1100

    Add debugging and implement parallel calls to retrieve route information

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const ptv = require("ptv-api");
+const async = require('async');
+
 const app = express();
 
 const devId = "1000649";
@@ -10,10 +12,8 @@ const ptvClient = ptv(devId, apiKey);
 
 app.get("/routesForStop", (req, res, next) => {
   if (!req.query.hasOwnProperty("stop_id")) {
-    res.sendStatus(404);
-    return;
+    return res.sendStatus(404);
   }
-
 
   // Hit PTV for departures, collect ids
   ptvClient
@@ -27,24 +27,45 @@ app.get("/routesForStop", (req, res, next) => {
       let stopRoutes = [];
       try {
         ptvRes.body.departures.forEach(departure => {
-          stopRoutes.push(departure.direction_id);
+          stopRoutes.push(departure.route_id);
         });
-      } catch (err) {
-        return next(err);
+      } catch(err) {
+        return next(err)
       }
 
       const uniqueStopRoutesSet = new Set(stopRoutes);
       const uniqueStopRoutes = Array.from(uniqueStopRoutesSet);
 
       // Hit PTV for route names, match against route ids
+      async.map(uniqueStopRoutes, getDirections, (err, directionsRes) => {
+        if (err) {
+          return next(err);
+        }
 
-      return res.send(uniqueStopRoutes);
+        // Flatten array of arrays into a 2d array
+        const directionsData = [].concat.apply([], directionsRes);
+        const uniqueDirections = new Set(directionsData);
+        const directions = Array.from(uniqueDirections);
 
-      // Create JSON structure
+        res.send(directions);
+      });
     })
     .catch(err => {
       return next(err);
     });
 });
+
+function getDirections(routeId, callback) {
+  ptvClient
+    .then(apis => {
+      return apis.Directions.Directions_ForRoute({ route_id: routeId });
+    })
+    .then(res => {
+      return callback(undefined, res.body.directions);
+    })
+    .catch(err => {
+      return callback(err);
+    });
+}
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
